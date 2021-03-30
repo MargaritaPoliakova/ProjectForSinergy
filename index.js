a4a34c7030283ad3c450ced306535c320468b8be
MargaritaPoliakova <margarita.03@list.ru>
2021-03-30 21:57:47 +0800

    !

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var Parser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
