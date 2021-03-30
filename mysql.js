fd41e66bb23fe44e2afe4f261c971b8ef565b8e3
MargaritaPoliakova <72750629+MargaritaPoliakova@users.noreply.github.com>
2021-03-30 22:30:23 +0800

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -1,23 +1,16 @@
-const http = require ('http');
-const fs = require('fs');
+const mysql = require('mysql')
 
-const server = http.createServer(function(req,res){
-    if (req.url === '/registration' || req.url === '/' ) {
-        res.writeHead(200, {'Conver-Type':'text/html, charset= utf8'});
-         fs.createReadStream(__dirname + '/html/registration.html', 'utf8').pipe(res);
-    }
-    else if (req.url === '/enter') {
-        res.writeHead(200, {'Conver-Type':'text/html, charset= utf8'});
-         fs.createReadStream(__dirname + '/html/enter.html', 'utf8').pipe(res);
-    }
-    else if (req.url === '/profile') {
-        res.writeHead(200, {'Conver-Type':'text/html, charset= utf8'});
-         fs.createReadStream(__dirname + '/html/profile.html', 'utf8').pipe(res);
-    }
-    else{
-        res.writeHead(404, {'Conver-Type':'text/html, charset= utf8'});
-         fs.createReadStream(__dirname + '/html/404.html', 'utf8').pipe(res);
-    }
+const connection = mysql.createConnection({
+host: "localhost",
+user: "root",
+database: "Mysql",
+password: "root"
 });
-
-server.listen(3000,'127.0.0.1')
+connection.connect(function(err){
+if (err) {
+return console.error("Ошибка: " + err.message);
+}
+else{
+console.log("Подключение к серверу MySQL успешно установлено");
+}
+}); 
